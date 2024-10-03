e701294438f8c9ed7164e84354799cbfc2f45ef3
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-10-03 17:56:41 +0200

    second commit

--- telemonitoringCHF/src/main/java/client/Client.java
+++ telemonitoringCHF/src/main/java/client/Client.java
@@ -10,6 +10,9 @@ package client;
  */
 public class Client {
     public static void main(String[] args) {
+        int i=1;
+        i++;
+        System.out.println(i);
         System.out.println("\n\nhola\n\n");
     }
 }

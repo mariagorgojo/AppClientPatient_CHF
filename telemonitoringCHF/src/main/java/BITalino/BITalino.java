16c959834b6b8d9d01128feddb1a55083567ceec
maria <maria@192.168.1.102>
2024-10-16 10:50:51 +0200

    bitalino package created

---
+++ telemonitoringCHF/src/main/java/BITalino/BITalino.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.BITalino to edit this template
+ */
+package BITalino;
+
+/**
+ *
+ * @author maria
+ */
+public class BITalino {
+    //hola
+    
+}

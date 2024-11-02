287b9164326e83a7debf7b7bc7f956fabb0d98aa
maria <maria@192.168.1.102>
2024-11-02 23:18:32 +0100

    pojos finished

--- telemonitoringCHF/src/main/java/pojos/Doctor.java
+++ telemonitoringCHF/src/main/java/pojos/Doctor.java
@@ -4,26 +4,76 @@
  */
 package pojos;
 
-class Doctor {
+import java.util.ArrayList;
+
+public class Doctor {
     
     // no estan todos los atributos RW
-    
-    	private String id;
+        private Integer id;
+    	private String dni;
 	private String name;
+        private String surname;
 	private Integer telephone;
         private String email;
+        private ArrayList<Patient> patients;
 
-    public Doctor() {
+     public Doctor() {
+        this.patients = new ArrayList<>();
+
+    }
+    public Doctor(String dni, String name, String surname, Integer telephone, String email) {
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.telephone = telephone;
+        this.email = email;
+        this.patients = new ArrayList<>();
+    }
+    
+        public Doctor(Integer id,String dni, String name, String surname, Integer telephone, String email) {
+        this.id= id;
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.telephone = telephone;
+        this.email = email;
+        this.patients = new ArrayList<>();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+    public Doctor(Integer id) {
         this.id = id;
     }
 
+    public Doctor(String dni) {
+        this.dni = dni;
+    }        
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    
+    
     public String getName() {
         return name;
     }
@@ -48,7 +98,20 @@ class Doctor {
         this.email = email;
     }
 
-	
-    
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(ArrayList<Patient> patients) {
+        this.patients = patients;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" + "dni=" + dni + ", name=" + name + ", surname=" + 
+                surname + ", telephone=" + telephone + ", email=" + email + 
+                ", patients=" + patients + '}';
+    }
+  
     
 }
